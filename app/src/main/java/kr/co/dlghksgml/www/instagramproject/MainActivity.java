d4926aff87c3caf8651ed21d283ffba4aa031d16
bacehee <bacehee@gmail.com>
2017-01-04 11:31:35 +0900

    first commit

---
+++ app/src/main/java/kr/co/dlghksgml/www/instagramproject/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.co.dlghksgml.www.instagramproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
